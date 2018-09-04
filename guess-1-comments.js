ef7d2ab8b34dea009649723c0fb33f322d41c4bd
BenWoodsBTC <42870843+BenWoodsBTC@users.noreply.github.com>
2018-09-04 11:34:28 -0400

    Comments entered

--- guess-1-comments.js
+++ guess-1-comments.js
@@ -1,6 +1,5 @@
 // Generates a random number between 0 and 1, then converts to an integer by rounding up
 var Answer = Math.round(Math.random()*100);
-// Sets the Turn variable to 0, initializing the variable 
 // A variable to set that amount of turns to 0
 var Turns=0;
 // Initializing the boolean variable Correct, setting the value to False
@@ -8,17 +7,28 @@ var Correct=false;
 // Adds 1 to the variable "Turns"
 Turns++;
 while (Correct==false) {
+// Initializes the variable "Guess" setting the value to user imput, telling the user what to imput
     var Guess = prompt("Guess my integer (between 0 and 100)!");
+// Checks if the guess is equal to the answer
     if (Guess == Answer) {
+// If equal, say "Correct" and number of turns        
         alert("CORRECT! You guessed it in "+Turns+" turns.");
+// set Correct equal to true to get out of the "while loop"
         Correct = true;
     }
+// check if the guess is greater than the answer    
     else if (Guess > Answer) {
+// give feedback, go lower        
         alert("Hint: Try a lower number!");
+// tells you if your number is lower than the answer        
     }
     else if (Guess < Answer) {
+// give feedback, go higher        
         alert("Hint: Try a higher number!");
+// tells you if your number is higher than the answer        
     }
     Turns++;
+// adds 1 to the "Turns" variable    
 }
 alert("Thank you for playing.");
+// the end of the program, telling the user "Thanks for playing"
